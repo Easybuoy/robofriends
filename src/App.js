1703b8eae13736df7cc25fb7055c11488920e71c
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-05-15 09:12:57 +0100

    [feature] robofriends

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
+import Scroll from './Scroll';
 import SearchBox from './SearchBox';
 import './App.css';
 class App extends Component{
     constructor(){
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''
         }
     }
+
+componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({robots: users}));
+    // this.setState({robots: robots});
+}
 
 onSearchChange = (event) =>{
     this.setState({searchField: event.target.value});
@@ -20,14 +27,21 @@ onSearchChange = (event) =>{
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
         });
-
-        return (
-            <div className="tc">
-            <h1 className="f2">RoboFriends</h1>
-            <SearchBox searchChange = {this.onSearchChange}/>
-            <CardList robots= {filteredRobots} />
-            </div>
-        ); 
+        
+       if(this.state.robots.length === 0){
+            return <h1 className="tc">Loading</h1>
+       }else{
+            return (
+                <div className="tc">
+                <h1 className="f2">RoboFriends</h1>
+                <SearchBox searchChange = {this.onSearchChange}/>
+                <Scroll>
+                <CardList robots= {filteredRobots} />
+                </Scroll>
+                </div>
+            ); 
+       }
+        
     }
 }
 
---
+++ src/Scroll.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Scroll = (props) => {
+    return (
+        <div style = {{ border: '5px solid black', overflow: 'scroll', height: '800px'}} >
+            {props.children}
+        </div>
+    );
+};
+
+export default Scroll;
